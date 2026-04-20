73425bdd078a851c9b2367195af8643ce6de705e
Kathillen <devkathillen@gmail.com>
2026-04-20 09:09:44 -0300

    subindo pro git

---
+++ front/src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import {AppLayout} from './pages/AppLayout'
+
+function App() {
+
+  return (
+    <>
+    <AppLayout title={'PlanejAI - Jovem!'}>
+      <h2>Aplicação para facilitar o controle financeiro</h2>
+    </AppLayout>
+    </>
+  )
+}
+
+export default App
